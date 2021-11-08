11826e5361643daac7dc55a00769c2cfd406daf8
Elena <elena.radchenko.9@mail.ru>
2021-11-08 23:49:18 +0300

    add new changes in file 1

--- Elena_Radchenko_dz1/dz_number1.py
+++ Elena_Radchenko_dz1/dz_number1.py
@@ -1,9 +1,9 @@
 duration = int(input("Введите продолжительность времени, в секундах: "))
 
-days = duration // (60 * 60 * 24)
-hours = (duration - days * (60 * 60 * 24))// (60 * 60)
-minutes = (duration - days * (60 * 60 * 24) - hours * (60 * 60)) // 60
-seconds = duration - days * (60 * 60 * 24) - hours * (60 * 60) - minutes * 60
+days = duration // 86400 % 24
+hours = duration // 3600 % 24
+minutes = duration // 60 % 60
+seconds = duration % 60
 print(f"Продолжительность времени равна: {days}дней, {hours}часов, {minutes}минут, {seconds}секунд")
 
 
